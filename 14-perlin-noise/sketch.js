cd641fea79ef03158dbd779a75c9a0711e1fd3a7
ilaria-castelli <61932397+ilaria-castelli@users.noreply.github.com>
2020-03-09 15:55:08 +0100

    Add files via upload

---
+++ 14-perlin-noise/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+}
+
+
+let noiseScale=0.02;
+
+function draw() {
+  background(0);
+  for (let x=0; x < width; x++) {
+    let noiseVal = noise((mouseX+x)*noiseScale, mouseY*noiseScale);
+    stroke(noiseVal*255);
+    line(x, mouseY+noiseVal*80, x, height);
+  }
+}
