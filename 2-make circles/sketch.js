deb016aeb5b854269ef4195f300461697b6a023b
ilaria-castelli <61932397+ilaria-castelli@users.noreply.github.com>
2020-03-09 16:35:14 +0100

    Add files via upload

---
+++ 2-make circles/sketch.js
@@ -0,0 +1,13 @@
+function preload(){
+  // put preload code here
+}
+
+function setup() {
+  createCanvas(windowWidth,windowHeight)
+  // put setup code here
+}
+
+function draw() {
+  // put drawing code here
+  ellipse(mouseX,mouseY,100,100);
+}
